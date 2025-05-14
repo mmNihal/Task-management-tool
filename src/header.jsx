7bfc1e3cd9d147784e5570c31abb52d293837007
mmNihal <xxxxmd309@gmail.com>
2025-05-15 00:24:10 +0600

    I've just change  the logo

--- src/header.jsx
+++ src/header.jsx
@@ -1,5 +1,5 @@
 // import frame from "./assets/n.svg";
-import mainLogo from "./assets/site-logo.png";
+import mainLogo from "./assets/mainlogo.png";
 
 export default function Header() {
   return (
